ac8b0e863986f1f121611af23c98d1e2973553ba
Francis Calizo <francisacalizo@gmail.com>
2020-03-24 12:30:56 -0400

    resize modal

--- src/components/gallery/Modal.js
+++ src/components/gallery/Modal.js
@@ -4,6 +4,28 @@ import Img from "gatsby-image"
 import styled from "styled-components"
 import ReactModal from "react-modal"
 
+const ModalStyles = {
+  overlay: {
+    zIndex: 20,
+    backgroundColor: "transparent",
+  },
+  content: {
+    backgroundColor: "rgba(0,0,0,0.75)",
+    height: "100%",
+    width: "100%",
+    top: 0,
+    left: 0,
+  },
+}
+
+const ImageContainer = styled.div`
+  display: block;
+`
+
+const Image = styled(Img)`
+  display: block;
+`
+
 const Modal = ({ showModal, handleCloseModal, modalImageName }) => {
   const modalData = useStaticQuery(graphql`
     query ModalQuery {
@@ -11,7 +33,7 @@ const Modal = ({ showModal, handleCloseModal, modalImageName }) => {
         edges {
           node {
             childImageSharp {
-              fixed(quality: 100, height: 600, width: 400) {
+              fixed(quality: 100, height: 800, width: 505) {
                 originalName
                 ...GatsbyImageSharpFixed
               }
@@ -30,33 +52,18 @@ const Modal = ({ showModal, handleCloseModal, modalImageName }) => {
 
   return (
     <Fragment>
-      <ReactModal
-        isOpen={showModal}
-        ariaHideApp={false}
-        contentLabel="Minimal Modal Example"
-        style={{
-          overlay: {
-            zIndex: 20,
-            backgroundColor: "transparent",
-          },
-          content: {
-            backgroundColor: "rgba(0,0,0,0.75)",
-            height: "100%",
-            width: "100%",
-            top: 0,
-            left: 0,
-          },
-        }}
-      >
-        {imageNode.current.length > 0 && (
-          <Img
-            fixed={imageNode.current[0].node.childImageSharp.fixed}
-            alt={
-              modalData.modalImages.edges[0].node.childImageSharp.fixed
-                .originalName
-            }
-          />
-        )}
+      <ReactModal isOpen={showModal} ariaHideApp={false} style={ModalStyles}>
+        <ImageContainer>
+          {imageNode.current.length > 0 && (
+            <Image
+              fixed={imageNode.current[0].node.childImageSharp.fixed}
+              alt={
+                modalData.modalImages.edges[0].node.childImageSharp.fixed
+                  .originalName
+              }
+            />
+          )}
+        </ImageContainer>
         <button onClick={handleCloseModal}>Close Modal</button>
       </ReactModal>
     </Fragment>
