e2c47511fa49eba39c0a2dfc85b1bb403904e254
Francis Calizo <francisacalizo@gmail.com>
2020-11-12 22:57:29 -0500

    fix: hotel info, FAQs, covid response, typos

--- src/components/faqs/data/Faqs.js
+++ src/components/faqs/data/Faqs.js
@@ -2,12 +2,18 @@ export const data = {
   title: "FAQs",
   rows: [
     {
+      title: `What should we expect given the current COVID-19 situation?`,
+      content: `We understand these times have been challenging. Your health and safety is important to us. If for any reason you and your party are uncomfortable attending our wedding, we please ask that you let us know on or before <span style="text-decoration: underline; font-weight: bold">January 15, 2021</span>.
+      <br><br>For those attending our wedding, we please ask that you practice social-distancing by reducing physical interaction between people as to help prevent the spread of COVID-19. We <strong>HIGHLY</strong> <strong>ENCOURAGE</strong> that you wear a mask when not eating or drinking.
+      `,
+    },
+    {
       title: `What should I wear?`,
       content: `We recommend dressing in anything from "Cocktail" to "Formal" and "Black Tie Optional" attire. More information on those descriptions can be found <a href="https://www.theknot.com/content/wedding-guest-attire-cheat-sheet" target="_blank" style="color:#ff7979;">here</a>.`,
     },
     {
       title: `What time and where is the reception and ceremony?`,
-      content: `More details to follow.`,
+      content: `The ceremony will be held at 12:00PM at Annunciation Catholic Church in Altamonte Springs. The reception will be held at 5:00PM at the Orlando Museum of Art.`,
     },
     {
       title: `Is this an indoor or outdoor event?`,
@@ -18,12 +24,20 @@ export const data = {
       content: `YES! The wedding would be called off if there wasn't.`,
     },
     {
-      title: `Where can I park?`,
-      content: `More details to follow.`,
-    },
-    {
       title: `Do you have any recommended hotels in the area?`,
-      content: `More details to follow.`,
+      content: `We do not have any hotel blocks reserved, but two great hotels that are 20 minutes from the ceremony and 10 mins from the reception are
+      <a
+        href="https://www.marriott.com/hotels/travel/mcodo-marriott-orlando-downtown/"
+        target="_blank"
+        style="color:#ff7979;"
+      >Marriott Downtown Orlando</a>
+      and
+      <a
+        href="https://www.ihg.com/crowneplaza/hotels/us/en/orlando/mcooa/hoteldetail"
+        target="_blank"
+        style="color:#ff7979;"
+      >Crowne Plaza Orlando-Downtown</a>
+      `,
     },
     {
       title: `Are you registered anywhere?`,
@@ -48,7 +62,7 @@ export const data = {
     },
     {
       title: `How can I RSVP? And when is the last day to do so?`,
-      content: `The last day to RSVP is <span style="text-decoration: underline; font-weight: bold">January 22, 2021</span>. To RSVP, click the button in the RSVP section labeled "Click here to RSVP" below. You will need the code provided in your wedding invitation in order to submit your RSVP electronically. Please email or call us if you have any questions!`,
+      content: `The last day to RSVP is <span style="text-decoration: underline; font-weight: bold">January 15, 2021</span>. To RSVP, click the button in the RSVP section labeled "Click here to RSVP" below. You will need the code provided in your wedding invitation in order to submit your RSVP electronically. Please email or call us if you have any questions!`,
     },
     {
       title: `What if I can't make it anymore?`,
